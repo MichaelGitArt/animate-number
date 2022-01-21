9f66b7fa184d0b33d6f344cc01ca682a56eb2524
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2022-01-21 23:06:44 +0200

    docs: add comments for animate parameters

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,13 +4,36 @@ export type SetValueCallback = (value: number) => void
 export type BezierParams = [number, number, number, number]
 
 export interface IParams {
+  /**
+   * start value
+   */
   from: number
+
+  /**
+   * end value
+   */
   to: number
+
+  /**
+   * milliseconds
+   */
   duration: number
+
+  /**
+   * bezier curve parameters
+   */
   bezier?: BezierParams
+
+  /**
+   * a callback that is called many times until the animation is complete.
+   * the current progress is passed as the first parameter.
+   */
   on: SetValueCallback
 }
 
 export interface IAnimateByIntervalParams extends IParams {
+  /**
+   * frames per second
+   */
   fps?: number
 }
