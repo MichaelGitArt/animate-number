8d54b380f449e83b19879415cff01b40b1c71582
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2022-01-22 23:54:42 +0200

    feat: make export for easingTypes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export type { IParams, IAnimateByIntervalParams, BezierParams } from './types'
 
 export { animate, animateUsingInterval, animateUsingRAF } from './animate'
+export { easingTypes } from './config'
